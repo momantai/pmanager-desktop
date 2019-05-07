b5ff4af64403fb6ab54e579630b4e9755509860b
Al <al>
2019-05-07 14:14:54 -0500

    Box new task

--- dist/importsandconfig.js
+++ dist/importsandconfig.js
@@ -17,8 +17,8 @@ console.log("¡Iniciando aplicación!")
     Configuration.
 */
 
-//let url = 'https://pmanagerd.mybluemix.net'
-let url = 'http://0.0.0.0:5000'
+let url = 'https://pmanagerd.mybluemix.net'
+//let url = 'http://0.0.0.0:5000'
 //let url = 'http://www.cloudmomantai.ml'
 
 var urltask = url + '/api/' + 'momantai' + '/' + 'nuevo' + '/task'
